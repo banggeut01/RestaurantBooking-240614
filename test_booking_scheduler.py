be533335114b3b3dd2dc4f6a94bf9a2717938be0
jinnie <genie121110@gmail.com>
2024-06-14 13:45:39 +0900

    [refactor] step12 - 통합 Test Double 로 대체

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -12,22 +12,13 @@ CUSTOMER_WITH_MAIL = Customer("Fake Name", "010-1234-5678", "test@test.com")
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
-class SundayBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour):
+class TestableBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, date_time: str):
         super().__init__(capacity_per_hour)
-
-    # override
-    def get_now(self):
-        return datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
-
-
-class MondayBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour):
-        super().__init__(capacity_per_hour)
+        self._date_time = date_time
 
     def get_now(self):
-        return datetime.strptime("2024/06/03 17:00", "%Y/%m/%d %H:%M")
-
+        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
 
 class BookingSchedulerTest(unittest.TestCase):
 
@@ -114,7 +105,7 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_현재날짜가_일요일인_경우_예약불가_예외처리(self):
         # arrange
-        self.booking_scheduler = SundayBookingScheduler(CAPACITY_PER_HOUR)
+        self.booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2021/03/28 17:00")
 
         # act and assert
         with self.assertRaises(ValueError):
@@ -124,7 +115,7 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_현재날짜가_일요일이_아닌경우_예약가능(self):
         # arrange
-        self.booking_scheduler = MondayBookingScheduler(CAPACITY_PER_HOUR)
+        self.booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2024/06/03 17:00")
 
         # act
         new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
